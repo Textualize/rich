62f1aacd41555c4a9d084a5b256e004d9c3ed2fc
Will McGugan <willmcgugan@gmail.com>
2019-11-27 18:29:22 +0000

    extra box drawing chars

--- rich/box.py
+++ rich/box.py
@@ -5,25 +5,23 @@ class Box:
     """Defines characters to render boxes."""
 
     def __init__(self, box: str) -> None:
-        box = box.strip().replace("\n", "")
+        line1, line2, line3, line4, line5, line6 = box.strip().splitlines()
+        self.top_left, self.top, self.top_divider, self.top_right = line1
+
+        self.left, _, self.vertical, self.right = line2
+
         (
-            self.top_left,
-            self.top,
-            self.top_cross,
-            self.top_right,
-            self.left,
-            _,
-            self.mid_vertical,
-            self.right,
-            self.mid_left,
-            self.mid_horizontal,
-            self.cross,
-            self.mid_right,
-            self.bottom_left,
-            self.bottom,
-            self.bottom_cross,
-            self.bottom_right,
-        ) = box
+            self.left_row,
+            self.horizontal,
+            self.horizontal_top_divider,
+            self.right_row,
+        ) = line3
+
+        _, _, self.cross, _ = line4
+
+        _, _, self.horizontal_bottom_divider, self.right_row = line5
+
+        self.bottom_left, self.bottom, self.bottom_divider, self.bottom_right = line6
 
     def __repr__(self) -> str:
         border_str = str(self).replace("\n", r"\n")
@@ -31,11 +29,11 @@ class Box:
 
     def __str__(self) -> str:
         return (
-            f"{self.top_left}{self.top}{self.top_cross}{self.top}{self.top_right}\n"
-            f"{self.left} {self.mid_vertical} {self.right}\n"
-            f"{self.mid_left}{self.mid_horizontal}{self.cross}{self.mid_horizontal}{self.mid_right}\n"
-            f"{self.left} {self.mid_vertical} {self.right}\n"
-            f"{self.bottom_left}{self.bottom}{self.bottom_cross}{self.bottom}{self.bottom_right}"
+            f"{self.top_left}{self.top}{self.top_divider}{self.top}{self.top_right}\n"
+            f"{self.left} {self.vertical} {self.right}\n"
+            f"{self.left_row}{self.horizontal}{self.cross}{self.horizontal}{self.right_row}\n"
+            f"{self.left} {self.vertical} {self.right}\n"
+            f"{self.bottom_left}{self.bottom}{self.bottom_divider}{self.bottom}{self.bottom_right}"
         )
 
     def get_top(self, width: int) -> str:
@@ -67,10 +65,12 @@ class Box:
 
 ASCII = Box(
     """
-+-++
++--+
 | ||
-+-++
-+-++
+|-+|
+|-+|
+|-+|
++--+
 """
 )
 
@@ -78,7 +78,9 @@ SQUARE = Box(
     """
 ┌─┬┐
 │ ││
+├─┴┤
 ├─┼┤
+├─┬┤
 └─┴┘
 """
 )
@@ -87,7 +89,9 @@ ROUNDED = Box(
     """
 ╭─┬╮
 │ ││
+├─┴┤
 ├─┼┤
+├─┬┤
 ╰─┴╯
 """
 )
@@ -96,7 +100,9 @@ HEAVY = Box(
     """
 ┏━┳┓
 ┃ ┃┃
+┣━┻┫
 ┣━╋┫
+┣━┳┫
 ┗━┻┛
 """
 )
@@ -105,7 +111,9 @@ HEAVY_EDGE = Box(
     """
 ┏━┯┓
 ┃ │┃
+┠─┴┨
 ┠─┼┨
+┠─┬┨
 ┗━┷┛
 """
 )
@@ -114,26 +122,43 @@ DOUBLE = Box(
     """
 ╔═╦╗
 ║ ║║
+╠═╩╣
 ╠═╬╣
-╚═╩╝   
+╠═╦╣
+╚═╩╝
 """
 )
-
 
 DOUBLE_EDGE = Box(
     """
 ╔═╤╗
 ║ │║
+╟─┴╢
 ╟─┼╢
+╟─┬╢
 ╚═╧╝
 """
 )
 
+
 if __name__ == "__main__":
+    print("ASCII")
     print(ASCII)
+
+    print("SQUARE")
     print(SQUARE)
+
+    print("ROUNDED")
     print(ROUNDED)
+
+    print("HEAVY")
     print(HEAVY)
+
+    print("HEAVY_EDGE")
     print(HEAVY_EDGE)
+
+    print("DOUBLE")
     print(DOUBLE)
+
+    print("DOUBLE_EDGE")
     print(DOUBLE_EDGE)
