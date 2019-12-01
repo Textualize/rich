ae2844e6f7b1ee900ea3f160d1d878e1dd762e26
Will McGugan <willmcgugan@gmail.com>
2019-12-01 22:17:30 +0000

    tables and padding

---
+++ rich/containers.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from typing import List, Literal, TypeVar, TYPE_CHECKING
+
+
+from .console import (
+    Console,
+    ConsoleOptions,
+    RenderResult,
+    RenderableType,
+)
+
+from .segment import Segment
+
+if TYPE_CHECKING:
+    from .text import Text
+
+
+T = TypeVar("T")
+
+
+class Renderables(List[T]):
+    """A list subclass which renders its contents to the console."""
+
+    def __console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        """Console render method to insert line-breaks."""
+        for renderable in self:
+            yield renderable
+
+
+class Lines(List["Text"]):
+    """A list subclass which can render to the console."""
+
+    def __console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        """Console render method to insert line-breaks."""
+        for line in self:
+            yield line
+            yield Segment("\n")
+
+    def justify(
+        self, width: int, align: Literal["left", "center", "right", "full"] = "left"
+    ) -> None:
+        """Pad each line with spaces to a given width.
+        
+        Args:
+            width (int): Number of characters per line.
+            
+        """
+        if align == "left":
+            for line in self:
+                line.pad_right(width - len(line.text))
+        elif align == "center":
+            for line in self:
+                line.pad_left((width - len(line.text)) // 2)
+                line.pad_right(width - len(line.text))
+        elif align == "right":
+            for line in self:
+                line.pad_left(width - len(line.text))
+        elif align == "full":
+            for line_index, line in enumerate(self):
+                if line_index == len(self) - 1:
+                    break
+                words = line.split(" ")
+                words_size = sum(len(word) for word in words)
+                num_spaces = len(words) - 1
+                spaces = [1 for _ in range(num_spaces)]
+                index = 0
+                while words_size + num_spaces < width:
+                    spaces[len(spaces) - index - 1] += 1
+                    num_spaces += 1
+                    index = (index + 1) % len(spaces)
+                tokens: List[Text] = []
+                index = 0
+                for index, word in enumerate(words):
+                    tokens.append(word)
+                    if index < len(spaces):
+                        tokens.append(Text(" " * spaces[index]))
+                    index += 1
+                self[line_index] = Text("").join(tokens)
---
+++ rich/padding.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import Union
+
+from .console import (
+    Console,
+    ConsoleOptions,
+    get_render_width,
+    RenderableType,
+    RenderResult,
+    RenderableWidth,
+)
+from .style import Style
+from .text import Text
+from .segment import Segment
+
+
+class Padding:
+    """Draw space around a console renderable."""
+
+    def __init__(
+        self,
+        renderable: RenderableType,
+        all_sides: int = None,
+        top: int = None,
+        right: int = None,
+        bottom: int = None,
+        left: int = None,
+        style: Union[str, Style] = "none",
+    ):
+        self.renderable = renderable
+        self.top = self.right = self.bottom = self.left = 0
+        if all_sides is not None:
+            self.top = self.right = self.bottom = self.left = all_sides
+        if top is not None:
+            self.top = top
+        if right is not None:
+            self.right = right
+        if bottom is not None:
+            self.bottom = bottom
+        if left is not None:
+            self.left = left
+        self.style = style
+
+    def __repr__(self) -> str:
+        return f"<padding {self.renderable!r} {self.top} {self.right} {self.bottom} {self.left}>"
+
+    def __console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+
+        style = console.get_style(self.style)
+        width = options.max_width
+        child_options = options.with_width(width - self.left - self.right)
+        lines = console.render_lines(self.renderable, child_options)
+        lines = Segment.set_shape(lines, child_options.max_width)
+
+        row = Segment(" " * width + "\n", style)
+        left = Segment(" " * self.left, style)
+        right = Segment(" " * self.right + "\n", style)
+        for _ in range(self.top):
+            yield row
+        for line in lines:
+            if self.left:
+                yield left
+            yield from line
+            if self.right:
+                yield right
+        for _ in range(self.bottom):
+            yield row
+
+    def __console_width__(self, max_width: int) -> RenderableWidth:
+        extra_width = self.left + self.right
+        width = get_render_width(self.renderable, max_width - extra_width) + extra_width
+        return RenderableWidth(width, width)
+
