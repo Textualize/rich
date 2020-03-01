6246b1de16f9e53ff2c072dbd8f7903b1cb14b74
Will McGugan <willmcgugan@gmail.com>
2020-03-01 14:45:44 +0000

    missed files

---
+++ rich/protocol.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+
+def is_renderable(test_renderable: Any) -> bool:
+    """Check if an object may be rendered by Rich."""
+    return (
+        isinstance(test_renderable, str)
+        or hasattr(test_renderable, "__console__")
+        or hasattr(test_renderable, "__rich__")
+    )
+
