cc0f1c97753dc015a01cf9caf10ee9d6eae28dc3
Will McGugan <willmcgugan@gmail.com>
2020-04-26 15:40:24 +0100

    simplify constrain

--- rich/constrain.py
+++ rich/constrain.py
@@ -11,13 +11,10 @@ class Constrain:
         width (int, optional): The maximum width (in characters) to render. Defaults to 80.
     """
 
-    def __init__(self, renderable: RenderableType, width: Optional[int] = 80) -> None:
+    def __init__(self, renderable: RenderableType, width: int = 80) -> None:
         self.renderable = renderable
         self.width = width
 
     def __console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
-        if self.width is None:
-            yield self.renderable
-        else:
-            child_options = options.update(width=min(self.width, options.max_width))
-            yield from console.render(self.renderable, child_options)
+        child_options = options.update(width=min(self.width, options.max_width))
+        yield from console.render(self.renderable, child_options)
