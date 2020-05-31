a81abc935bd69494f10aee68260b3feecea86daa
Will McGugan <willmcgugan@gmail.com>
2020-05-31 15:32:27 +0100

    tidy example

--- examples/columns.py
+++ examples/columns.py
@@ -1,20 +1,19 @@
 import json
 from urllib.request import urlopen
 
-
 from rich import print
 from rich.columns import Columns
 from rich.panel import Panel
 
-users = json.loads(urlopen("https://randomuser.me/api/?results=30").read())["results"]
-print(users)
-
 
 def get_content(user):
+    """Extract text from user dict."""
     country = user["location"]["country"]
     name = f"{user['name']['first']} {user['name']['last']}"
     return f"[b]{name}[/b]\n[yellow]{country}"
 
 
+users = json.loads(urlopen("https://randomuser.me/api/?results=30").read())["results"]
+print(users)
 user_renderables = [Panel(get_content(user), expand=False) for user in users]
 print(Columns(user_renderables))
