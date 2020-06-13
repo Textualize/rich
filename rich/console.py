d781c3db929beb7af47c55d7f4b43cb2250587a0
Will McGugan <willmcgugan@gmail.com>
2020-06-13 15:09:06 +0100

    docstrings

--- rich/console.py
+++ rich/console.py
@@ -236,11 +236,21 @@ class ConsoleThreadLocals(threading.local):
 
 
 class RenderHook:
-    """Provides hooks in to render process."""
+    """Provides hooks in to the render process."""
 
     def process_renderables(
         self, renderables: List[ConsoleRenderable]
     ) -> List[ConsoleRenderable]:
+        """Called with a list of objects to render.
+
+        This method can return a new list of renderables, or modify and return the same list.
+
+        Args:
+            renderables (List[ConsoleRenderable]): A number of renderable objects.
+
+        Returns:
+            List[ConsoleRenderable]: A replacement list of renderables.
+        """
         return renderables
 
 
