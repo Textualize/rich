7b28c5dcd43b1215941bfa3710e98ebcc13b8717
Will McGugan <willmcgugan@gmail.com>
2020-06-15 12:05:47 +0100

    Test fix

--- tests/render.py
+++ tests/render.py
@@ -4,12 +4,12 @@ import re
 from rich.console import Console, RenderableType
 
 
-re_link_ids = re.compile(r"id=\d*?;")
+re_link_ids = re.compile(r"id=\d*?;.*?\x1b")
 
 
 def replace_link_ids(render: str) -> str:
-    """Link IDs have a random ID which is a problem for reproducable tests."""
-    return re_link_ids.sub("id=0", render)
+    """Link IDs have a random ID and system path which is a problem for reproducable tests."""
+    return re_link_ids.sub("id=0;foo\x1b", render)
 
 
 def render(renderable: RenderableType) -> str:
