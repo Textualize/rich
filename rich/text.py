a0db27dc3d593c7ad3a4cc22bd707d50cd7c9747
Will McGugan <willmcgugan@gmail.com>
2020-07-10 23:06:05 +0100

    Doc fix

--- rich/text.py
+++ rich/text.py
@@ -215,7 +215,7 @@ class Text(JupyterMixin):
         end: str = "\n",
         tab_size: int = 8,
     ) -> "Text":
-        """Construct a text instance by combining a sequence of strings with optional styles.
+        r"""Construct a text instance by combining a sequence of strings with optional styles.
         The positional arguments should be either strings, or a tuple of string + style.        
 
         Args:            
