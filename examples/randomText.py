9bda72c1d535e1bce7f29c92bbf06c7a6c3fabe1
NICKY B <nickblackburn02@gmail.com>
2020-10-04 16:30:50 -0400

    added random text iwth array

---
+++ examples/randomText.py
@@ -0,0 +1,19 @@
+
+"""
+Randomly Prints Names in random Colors to Show examples of how to use randbow text in an Text Generator.
+"""
+from random import randint
+
+from rich import print
+from rich.highlighter import Highlighter
+
+class RainbowHighlighter(Highlighter):
+    def highlight(self, text):
+        for index in range(len(text)):
+            text.stylize(f"color({randint(16, 255)})", index, index + 1)
+i = ["Harland Feeley","Rebbecca Lovins","Josephina Lippincott","Eboni Huwe","Allyson Trombley","Jordan Brisbin","Chloe Goll","Rodney Augustin","Noemi Cusumano","Agustina Buttner"]
+
+rainbow = RainbowHighlighter()
+
+for x in i:
+    print(rainbow(x)+"\n")
