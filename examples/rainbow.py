73ec0db8a11355f5fa11b61b5ace996643d4617e
Jamesbond8002 <72294116+Jamesbond8002@users.noreply.github.com>
2020-10-04 12:27:45 +0530

    Update rainbow.py
    Made The Code Look Cleaner, Professional and Easy to Understand.
    (For Hacktoberfest, Please Accept)
    Thank You,
    -James

--- examples/rainbow.py
+++ examples/rainbow.py
@@ -4,17 +4,25 @@ This example demonstrates how to write a custom highlighter.
 
 """
 
-from random import randint
+# Note: You Must Install Rich First
 
+# All Imports:
+from random import randint
 from rich import print
 from rich.highlighter import Highlighter
 
-
+# Creating The Rainbow Highlighter Class:
 class RainbowHighlighter(Highlighter):
+
+    # Defining The Highlight Function:
     def highlight(self, text):
+
+        # Chooses A Random Colour For Each Letter In The String:
         for index in range(len(text)):
             text.stylize(f"color({randint(16, 255)})", index, index + 1)
 
-
+# Calling The Rainbow Highlighter Class:
 rainbow = RainbowHighlighter()
+
+# Printing The Text That Has To Be Rainbow Highlighted:
 print(rainbow("I must not fear. Fear is the mind-killer."))
