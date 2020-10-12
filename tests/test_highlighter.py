75930e782623e717f1cebeb6c5108de0c083be19
Will McGugan <willmcgugan@gmail.com>
2020-10-12 18:50:26 +0100

    more tests

--- tests/test_highlighter.py
+++ tests/test_highlighter.py
@@ -13,6 +13,7 @@ def test_wrong_type():
 
 
 highlight_tests = [
+    (" ", []),
     (
         "<foo>",
         [
@@ -21,6 +22,16 @@ highlight_tests = [
             Span(4, 5, "repr.tag_end"),
         ],
     ),
+    (
+        "False True None",
+        [
+            Span(0, 5, "repr.bool_false"),
+            Span(6, 10, "repr.bool_true"),
+            Span(11, 15, "repr.none"),
+        ],
+    ),
+    ("foo=bar", [Span(0, 3, "repr.attrib_name"), Span(4, 7, "repr.attrib_value")]),
+    ('foo="bar"', [Span(0, 3, "repr.attrib_name"), Span(4, 9, "repr.attrib_value")]),
     ("( )", [Span(0, 1, "repr.brace"), Span(2, 3, "repr.brace")]),
     ("[ ]", [Span(0, 1, "repr.brace"), Span(2, 3, "repr.brace")]),
     ("{ }", [Span(0, 1, "repr.brace"), Span(2, 3, "repr.brace")]),
@@ -28,6 +39,7 @@ highlight_tests = [
     (" 1.2 ", [Span(1, 4, "repr.number")]),
     (" 0xff ", [Span(1, 5, "repr.number")]),
     (" /foo ", [Span(1, 2, "repr.path"), Span(2, 5, "repr.filename")]),
+    (" /foo/bar.html ", [Span(1, 6, "repr.path"), Span(6, 14, "repr.filename")]),
     ("01-23-45-67-89-AB", [Span(0, 17, "repr.eui48")]),  # 6x2 hyphen
     ("01-23-45-FF-FE-67-89-AB", [Span(0, 23, "repr.eui64")]),  # 8x2 hyphen
     ("01:23:45:67:89:AB", [Span(0, 17, "repr.ipv6")]),  # 6x2 colon
@@ -38,6 +50,16 @@ highlight_tests = [
     ("ED-ED-ED-ED-ED-ED", [Span(0, 17, "repr.eui48")]),  # uppercase
     ("Ed-Ed-Ed-Ed-Ed-Ed", [Span(0, 17, "repr.eui48")]),  # mixed case
     ("0-00-1-01-2-02", [Span(0, 14, "repr.eui48")]),  # dropped zero
+    ("https://example.org", [Span(0, 19, "repr.url")]),
+    ("http://example.org", [Span(0, 18, "repr.url")]),
+    ("No place like 127.0.0.1", [Span(14, 23, "repr.ipv4")]),
+    ("''", [Span(0, 2, "repr.str")]),
+    ("'hello'", [Span(0, 7, "repr.str")]),
+    ("'''hello'''", [Span(0, 11, "repr.str")]),
+    ('""', [Span(0, 2, "repr.str")]),
+    ('"hello"', [Span(0, 7, "repr.str")]),
+    ('"""hello"""', [Span(0, 11, "repr.str")]),
+    ("\\'foo'", []),
 ]
 
 
@@ -47,4 +69,5 @@ def test_highlight_regex(test: str, spans: List[Span]):
     text = Text(test)
     highlighter = ReprHighlighter()
     highlighter.highlight(text)
+    print(text.spans)
     assert text.spans == spans
