e32a715728fe272c25009c42f780f4af47ae2380
Will McGugan <willmcgugan@gmail.com>
2020-10-16 12:16:20 +0100

    simplify

--- examples/bars.py
+++ examples/bars.py
@@ -14,5 +14,5 @@ SIZE = 40
 for row in range(SIZE):
     y = (row / SIZE) * 2 - 1
     x = math.sqrt(1 - y * y)
-    bar = Bar(1.0, width=SIZE * 2, begin=1 - x, end=x, color="red")
+    bar = Bar(1, width=SIZE * 2, begin=1 - x, end=x, color="red")
     print(bar)
