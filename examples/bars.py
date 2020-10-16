88bc65df2b3e5a9a2368aaec0f6348509172cacd
Will McGugan <willmcgugan@gmail.com>
2020-10-16 15:58:46 +0100

    fix bars example

--- examples/bars.py
+++ examples/bars.py
@@ -1,10 +1,11 @@
 """
 
-Use Bar to renderer a sort-of cirlce.
+Use Bar to renderer a sort-of circle.
 
 """
 import math
 
+from rich.align import Align
 from rich.bar import Bar
 from rich.color import Color
 from rich import print
@@ -13,8 +14,8 @@ from rich import print
 SIZE = 40
 
 for row in range(SIZE):
-    y = (row / SIZE) * 2 - 1
+    y = (row / (SIZE - 1)) * 2 - 1
     x = math.sqrt(1 - y * y)
     color = Color.from_rgb((y + 1) * 127, 0, 0)
-    bar = Bar(1, width=SIZE * 2, begin=1 - x, end=x, color=color)
-    print(bar)
+    bar = Bar(2, width=SIZE * 2, begin=1 - x, end=1 + x, color=color)
+    print(Align.center(bar))
