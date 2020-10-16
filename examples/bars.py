ddea7878a9c31c943e14db60a573eb6d3b196c12
Will McGugan <willmcgugan@gmail.com>
2020-10-16 12:32:15 +0100

    prettier bars

--- examples/bars.py
+++ examples/bars.py
@@ -6,6 +6,7 @@ Use Bar to renderer a sort-of cirlce.
 import math
 
 from rich.bar import Bar
+from rich.color import Color
 from rich import print
 
 
@@ -14,5 +15,6 @@ SIZE = 40
 for row in range(SIZE):
     y = (row / SIZE) * 2 - 1
     x = math.sqrt(1 - y * y)
-    bar = Bar(1, width=SIZE * 2, begin=1 - x, end=x, color="red")
+    color = Color.from_rgb((y + 1) * 127, 0, 0)
+    bar = Bar(1, width=SIZE * 2, begin=1 - x, end=x, color=color)
     print(bar)
