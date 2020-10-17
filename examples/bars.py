75fe9c458d164104b0de9f05ad8832a294ea0841
Will McGugan <willmcgugan@gmail.com>
2020-10-17 15:54:00 +0100

    example fix

--- examples/bars.py
+++ examples/bars.py
@@ -16,6 +16,6 @@ SIZE = 40
 for row in range(SIZE):
     y = (row / (SIZE - 1)) * 2 - 1
     x = math.sqrt(1 - y * y)
-    color = Color.from_rgb((y + 1) * 127, 0, 0)
+    color = Color.from_rgb((1 + y) * 127.5, 0, 0)
     bar = Bar(2, width=SIZE * 2, begin=1 - x, end=1 + x, color=color)
     print(Align.center(bar))
