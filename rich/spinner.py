4823297d56bb5e0f9ee891553b0e237bc0143ba9
Will McGugan <willmcgugan@gmail.com>
2020-12-05 16:23:14 +0000

    spinner command

--- rich/spinner.py
+++ rich/spinner.py
@@ -69,15 +69,21 @@ if __name__ == "__main__":  # pragma: no cover
     from time import sleep
 
     from .columns import Columns
+    from .panel import Panel
     from .live import Live
 
     all_spinners = Columns(
         [
             Spinner(spinner_name, text=Text(repr(spinner_name), style="green"))
-            for spinner_name in SPINNERS.keys()
-        ]
+            for spinner_name in sorted(SPINNERS.keys())
+        ],
+        column_first=True,
+        expand=True,
     )
 
-    with Live(all_spinners, refresh_per_second=20) as live:
+    with Live(
+        Panel(all_spinners, title="Spinners", border_style="blue"),
+        refresh_per_second=20,
+    ) as live:
         while True:
             sleep(0.1)
