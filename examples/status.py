d180781465fc93e526be759e3cdb072c5afeaeb7
Will McGugan <willmcgugan@gmail.com>
2020-12-10 17:25:13 +0000

    formatting

--- examples/status.py
+++ examples/status.py
@@ -9,6 +9,5 @@ tasks = [f"task {n}" for n in range(1, 11)]
 with console.status("[bold green]Working on tasks...") as status:
     while tasks:
         task = tasks.pop(0)
-        sleep(1)                
+        sleep(1)
         console.log(f"{task} complete")
-
