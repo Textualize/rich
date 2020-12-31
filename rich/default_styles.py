481f30f237efb55df2bdc6d6385e95736227bd07
Will McGugan <willmcgugan@gmail.com>
2020-12-31 17:53:11 +0000

    fix encoding

--- rich/default_styles.py
+++ rich/default_styles.py
@@ -99,6 +99,7 @@ DEFAULT_STYLES: Dict[str, Style] = {
     "table.cell": Style.null(),
     "table.title": Style(italic=True),
     "table.caption": Style(italic=True, dim=True),
+    "traceback.error": Style(dim=True, color="red", bold=True),
     "traceback.border.syntax_error": Style(color="bright_red"),
     "traceback.border": Style(color="red"),
     "traceback.text": Style.null(),
