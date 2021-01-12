d4d087c14f8c7d227321a5f0ffa25e10a0751421
Nitin George Cherian <realnitinworks@gmail.com>
2021-01-12 12:10:32 +0530

    fix text indentation in docstring

--- rich/prompt.py
+++ rich/prompt.py
@@ -16,8 +16,8 @@ class InvalidResponse(PromptError):
     """Exception to indicate a response was invalid. Raise this within process_response() to indicate an error
     and provide an error message.
 
-        Args:
-            message (Union[str, Text]): Error message.
+    Args:
+        message (Union[str, Text]): Error message.
     """
 
     def __init__(self, message: TextType) -> None:
