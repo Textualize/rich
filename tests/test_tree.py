a13b2a9d751bbfe4dec6d9d5bf1083cacb13349a
Will McGugan <willmcgugan@gmail.com>
2021-01-20 17:48:41 +0000

    windows tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,3 +1,7 @@
+import sys
+
+import pytest
+
 from rich.console import Console
 from rich.measure import Measurement
 from rich.tree import Tree
@@ -54,6 +58,7 @@ def test_render_ascii():
     assert result == expected
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="different on Windows")
 def test_render():
     tree = Tree("foo")
     tree.add("bar", style="italic")
@@ -68,6 +73,24 @@ def test_render():
     result = console.end_capture()
     print(repr(result))
     expected = "foo                 \n├── \x1b[3mbar\x1b[0m\x1b[3m             \x1b[0m\n\x1b[44m├── \x1b[0m\x1b[44mbaz\x1b[0m\x1b[44m             \x1b[0m\n\x1b[44m│   \x1b[0m\x1b[31;44m┣━━ \x1b[0m\x1b[44m1\x1b[0m\x1b[44m           \x1b[0m\n\x1b[44m│   \x1b[0m\x1b[31;44m┗━━ \x1b[0m\x1b[44m2\x1b[0m\x1b[44m           \x1b[0m\n└── egg             \n"
+    assert result == expected
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows specific")
+def test_render():
+    tree = Tree("foo")
+    tree.add("bar", style="italic")
+    baz_tree = tree.add("baz", guide_style="bold red", style="on blue")
+    baz_tree.add("1")
+    baz_tree.add("2")
+    tree.add("egg")
+
+    console = Console(width=20, force_terminal=True, color_system="standard")
+    console.begin_capture()
+    console.print(tree)
+    result = console.end_capture()
+    print(repr(result))
+    expected = "foo                 \n├── \x1b[3mbar\x1b[0m\x1b[3m             \x1b[0m\n\x1b[44m├── \x1b[0m\x1b[44mbaz\x1b[0m\x1b[44m             \x1b[0m\n\x1b[44m│   \x1b[0m\x1b[31;44m├── \x1b[0m\x1b[44m1\x1b[0m\x1b[44m           \x1b[0m\n\x1b[44m│   \x1b[0m\x1b[31;44m└── \x1b[0m\x1b[44m2\x1b[0m\x1b[44m           \x1b[0m\n└── egg             \n"
     assert result == expected
 
 
