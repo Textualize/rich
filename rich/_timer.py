05843f6f69214a5ad94c880179d52665528e5494
Nathan Page <nathanrpage97@gmail.com>
2021-05-09 15:02:50 -0700

    update timer to generator

--- rich/_timer.py
+++ rich/_timer.py
@@ -6,11 +6,11 @@ Timer context manager, only used in debug.
 from time import time
 
 import contextlib
-from typing import Iterator
+from typing import Generator
 
 
 @contextlib.contextmanager
-def timer(subject: str = "time") -> Iterator[None]:
+def timer(subject: str = "time") -> Generator[None, None, None]:
     """print the elapsed time. (only used in debugging)"""
     start = time()
     yield
