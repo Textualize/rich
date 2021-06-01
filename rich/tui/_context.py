803651999e50ac2df1a60f1c0dd85729d7b556ce
Will McGugan <willmcgugan@gmail.com>
2021-06-01 17:13:43 +0100

    remote tui submodule

--- rich/tui/_context.py
+++
@@ -1,8 +0,0 @@
-from typing import TYPE_CHECKING
-
-from contextvars import ContextVar
-
-if TYPE_CHECKING:
-    from .app import App
-
-active_app: ContextVar["App"] = ContextVar("active_app")
--- rich/tui/widgets/placeholder.py
+++
@@ -1,9 +0,0 @@
-from ..widget import Widget
-
-from rich.repr import RichReprResult
-
-
-class Placeholder(Widget):
-    def __rich_repr__(self) -> RichReprResult:
-        yield "name", self.name
-        yield "mouse_over", self.mouse_over
