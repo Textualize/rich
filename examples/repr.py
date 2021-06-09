932c451dd9c66b8938676105d0ffc1823f131cf8
Will McGugan <willmcgugan@gmail.com>
2021-06-09 18:28:10 +0100

    repr example

--- examples/repr.py
+++ examples/repr.py
@@ -1,12 +1,13 @@
+from rich.repr import rich_repr
+
+
+@rich_repr
 class Bird:
     def __init__(self, name, eats=None, fly=True, extinct=False):
         self.name = name
         self.eats = list(eats) if eats else []
         self.fly = fly
         self.extinct = extinct
-
-    def __repr__(self):
-        return f"Bird({self.name!r}, eats={self.eats!r}, fly={self.fly!r}, extinct={self.extinct!r})"
 
     def __rich_repr__(self):
         yield self.name
@@ -15,11 +16,13 @@ class Bird:
         yield "extinct", self.extinct, False
 
     __rich_repr__.angular = True
-    
-from rich import print    
+
+
+from rich import print
+
 BIRDS = {
     "gull": Bird("gull", eats=["fish", "chips", "ice cream", "sausage rolls"]),
     "penguin": Bird("penguin", eats=["fish"], fly=False),
-    "dodo": Bird("dodo", eats=["fruit"], fly=False, extinct=True)
+    "dodo": Bird("dodo", eats=["fruit"], fly=False, extinct=True),
 }
 print(BIRDS)
