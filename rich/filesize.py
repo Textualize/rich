08b2a6441eca5a4399e8d5e2751cee764291e065
Pete Davison <pd93.uk@outlook.com>
2021-07-09 09:57:13 +0000

    Add precision and separator optional params to decimal()

--- rich/filesize.py
+++ rich/filesize.py
@@ -16,7 +16,7 @@ __all__ = ["decimal"]
 from typing import Iterable, List, Tuple
 
 
-def _to_str(size: int, suffixes: Iterable[str], base: int) -> str:
+def _to_str(size: int, suffixes: Iterable[str], base: int, precision: int = 1, separator: str = " ") -> str:
     if size == 1:
         return "1 byte"
     elif size < base:
@@ -26,7 +26,7 @@ def _to_str(size: int, suffixes: Iterable[str], base: int) -> str:
         unit = base ** i
         if size < unit:
             break
-    return "{:,.1f} {}".format((base * size / unit), suffix)
+    return "{:,.{precision}f}{separator}{}".format((base * size / unit), suffix, precision=precision, separator=separator)
 
 
 def pick_unit_and_suffix(size: int, suffixes: List[str], base: int) -> Tuple[int, str]:
@@ -38,7 +38,7 @@ def pick_unit_and_suffix(size: int, suffixes: List[str], base: int) -> Tuple[int
     return unit, suffix
 
 
-def decimal(size: int) -> str:
+def decimal(size: int, precision: int = 1, separator: str = " ") -> str:
     """Convert a filesize in to a string (powers of 1000, SI prefixes).
 
     In this convention, ``1000 B = 1 kB``.
@@ -50,6 +50,8 @@ def decimal(size: int) -> str:
 
     Arguments:
         int (size): A file size.
+        int (precision): The number of decimal places to include (default = 1).
+        str (separator): The string to separate the value from the units (default = " ").
 
     Returns:
         `str`: A string containing a abbreviated file size and units.
@@ -57,6 +59,8 @@ def decimal(size: int) -> str:
     Example:
         >>> filesize.decimal(30000)
         '30.0 kB'
+        >>> filesize.decimal(30000, precision=2, separator="")
+        '30.00kB'
 
     """
-    return _to_str(size, ("kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"), 1000)
+    return _to_str(size, ("kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"), 1000, precision=precision, separator=separator)
