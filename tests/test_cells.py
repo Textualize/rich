8a74a97563dad3437ce537ed07dc72e031c54638
Will McGugan <willmcgugan@gmail.com>
2021-11-13 09:04:12 +0000

    fix infinite loop

--- tests/test_cells.py
+++ tests/test_cells.py
@@ -9,3 +9,15 @@ def test_set_cell_size():
     assert cells.set_cell_size("😽😽", 3) == "😽 "
     assert cells.set_cell_size("😽😽", 2) == "😽"
     assert cells.set_cell_size("😽😽", 1) == " "
+
+
+def test_set_cell_size_infinite():
+    for size in range(38):
+        assert (
+            cells.cell_len(
+                cells.set_cell_size(
+                    "เป็นเกมที่ต้องมีความอดทนมากที่สุดตั้งเเต่เคยเล่นมา", size
+                )
+            )
+            == size
+        )
