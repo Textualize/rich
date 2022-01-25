15e72ab4332eef81ed4e834633c700dc13d17cae
Darren Burns <darrenb900@gmail.com>
2022-01-25 10:54:49 +0000

    Tidying up Syntax.guess_lexer, adding test

--- rich/syntax.py
+++ rich/syntax.py
@@ -322,8 +322,12 @@ class Syntax(JupyterMixin):
 
     @classmethod
     def guess_lexer(cls, path: AnyStr, code: Optional[str] = None) -> str:
-        """Guess the name of the Pygments lexer to use based on path and optional string of code.
-        Initially looks at file extension and falls back to analysing the code, if supplied.
+        """Guess the alias of the Pygments lexer to use based on a path and an optional string of code.
+        If code is supplied, it will use a combination of the code and the filename to determine the
+        best lexer to use. For example, if the file is ``index.html`` and the file contains Django
+        templating syntax, then "html+django" will be returned. If the file is ``index.html``, and no
+        templating language is used, the "html" lexer will be used. If no string of code
+        is supplied, the lexer will be chosen based on the file extension..
 
         Args:
              path (AnyStr): The path to the file containing the code you wish to know the lexer for.
@@ -333,21 +337,29 @@ class Syntax(JupyterMixin):
         Returns:
             str: The name of the Pygments lexer that best matches the supplied path/code.
         """
-        lexer = None
+        lexer: Optional[Lexer] = None
         lexer_name = "default"
-        try:
-            _, ext = os.path.splitext(path)
-            if ext:
-                extension = ext.lstrip(".").lower()
-                lexer = get_lexer_by_name(extension)
-                lexer_name = lexer.name
-        except ClassNotFound:
-            pass
-        if code and lexer is None:
+        if code:
             try:
-                lexer_name = guess_lexer_for_filename(path, code).name
+                lexer: Lexer = guess_lexer_for_filename(path, code)
             except ClassNotFound:
                 pass
+
+        if not lexer:
+            try:
+                _, ext = os.path.splitext(path)
+                if ext:
+                    extension = ext.lstrip(".").lower()
+                    lexer = get_lexer_by_name(extension)
+            except ClassNotFound:
+                pass
+
+        if lexer:
+            if lexer.aliases:
+                lexer_name = lexer.aliases[0]
+            else:
+                lexer_name = lexer.name
+
         return lexer_name
 
     def _get_base_style(self) -> Style:
