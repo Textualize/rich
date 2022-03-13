866307358f21a727dced87aed3ec2aa34230fefd
Martin Larralde <martin.larralde@embl.de>
2022-03-13 19:21:18 +0100

    Make `Progress.open` have a signature compatible with `open`

--- rich/progress.py
+++ rich/progress.py
@@ -1,4 +1,5 @@
 import io
+import warnings
 from abc import ABC, abstractmethod
 from collections import deque
 from collections.abc import Sized
@@ -342,6 +343,10 @@ def wrap_file(
 def open(
     file: Union[str, "PathLike[str]", bytes],
     mode: str = "rb",
+    buffering: int = -1,
+    encoding: Optional[str] = None,
+    errors: Optional[str] = None,
+    newline: Optional[str] = None,
     total: Optional[int] = None,
     description: str = "Reading...",
     auto_refresh: bool = True,
@@ -355,13 +360,16 @@ def open(
     pulse_style: StyleType = "bar.pulse",
     update_period: float = 0.1,
     disable: bool = False,
-    encoding: Optional[str] = None,
 ) -> ContextManager[BinaryIO]:
     """Read bytes from a file while tracking progress.
 
     Args:
         path (Union[str, PathLike[str], BinaryIO]): The path to the file to read, or a file-like object in binary mode.
         mode (str): The mode to use to open the file. Only supports "r", "rb" or "rt".
+        buffering (int): The buffering strategy to use, see :func:`io.open`.
+        encoding (str, optional): The encoding to use when reading in text mode, see :func:`io.open`.
+        errors (str, optional): The error handling strategy for decoding errors, see :func:`io.open`.
+        newline (str, optional): The strategy for handling newlines in text mode, see :func:`io.open`
         total: (int, optional): Total number of bytes to read. Must be provided if reading from a file handle. Default for a path is os.stat(file).st_size.
         description (str, optional): Description of task show next to progress bar. Defaults to "Reading".
         auto_refresh (bool, optional): Automatic refresh, disable to force a refresh after each iteration. Default is True.
@@ -409,9 +417,12 @@ def open(
     reader = progress.open(
         file,
         mode=mode,
+        buffering=buffering,
+        encoding=encoding,
+        errors=errors,
+        newline=newline,
         total=total,
         description=description,
-        encoding=encoding,
     )
     return _ReadContext(progress, reader)
 
@@ -1104,20 +1115,26 @@ class Progress(JupyterMixin):
         self,
         file: Union[str, "PathLike[str]", bytes],
         mode: str = "r",
+        buffering: int = -1,
+        encoding: Optional[str] = None,
+        errors: Optional[str] = None,
+        newline: Optional[str] = None,
         total: Optional[int] = None,
         task_id: Optional[TaskID] = None,
         description: str = "Reading...",
-        encoding: Optional[str] = None,
     ) -> BinaryIO:
         """Track progress while reading from a binary file.
 
         Args:
             path (Union[str, PathLike[str]]): The path to the file to read.
             mode (str): The mode to use to open the file. Only supports "r", "rb" or "rt".
+            buffering (int): The buffering strategy to use, see :func:`io.open`.
+            encoding (str, optional): The encoding to use when reading in text mode, see :func:`io.open`.
+            errors (str, optional): The error handling strategy for decoding errors, see :func:`io.open`.
+            newline (str, optional): The strategy for handling newlines in text mode, see :func:`io.open`.
             total (int, optional): Total number of bytes to read. If none given, os.stat(path).st_size is used.
             task_id (TaskID): Task to track. Default is new task.
             description (str, optional): Description of task, if new task is created.
-            encoding (str, optional): The encoding to use when reading in text mode.
 
         Returns:
             BinaryIO: A readable file-like object in binary mode.
@@ -1125,6 +1142,25 @@ class Progress(JupyterMixin):
         Raises:
             ValueError: When an invalid mode is given.
         """
+        # normalize the mode (always rb, rt)
+        _mode = "".join(sorted(mode, reverse=False))
+        if _mode not in ("br", "rt", "r"):
+            raise ValueError("invalid mode {!r}".format(mode))
+
+        # patch buffering to provide the same behaviour as the builtin `open`
+        line_buffering = buffering == 1
+        if _mode == "br" and buffering == 1:
+            warnings.warn(
+                "line buffering (buffering=1) isn't supported in binary mode, the default buffer size will be used",
+                RuntimeWarning,
+            )
+            buffering = -1
+        elif _mode == "rt" or _mode == "r":
+            if buffering == 0:
+                raise ValueError("can't have unbuffered text I/O")
+            elif buffering == 1:
+                buffering = -1
+
         # attempt to get the total with `os.stat`
         if total is None:
             total = stat(file).st_size
@@ -1135,17 +1171,19 @@ class Progress(JupyterMixin):
         else:
             self.update(task_id, total=total)
 
-        # normalize the mode (always rb, rt)
-        _mode = "".join(sorted(mode, reverse=False))
-        if _mode not in ("br", "rt", "r"):
-            raise ValueError("invalid mode {!r}".format(mode))
-
         # open the file in binary mode,
-        reader = _Reader(io.open(file, "rb"), self, task_id, close_handle=True)
+        handle = io.open(file, "rb", buffering=buffering)
+        reader = _Reader(handle, self, task_id, close_handle=True)
 
         # wrap the reader in a `TextIOWrapper` if text mode
         if mode == "r" or mode == "rt":
-            reader = io.TextIOWrapper(reader, encoding=encoding)
+            reader = io.TextIOWrapper(
+                reader,
+                encoding=encoding,
+                errors=errors,
+                newline=newline,
+                line_buffering=line_buffering,
+            )
 
         return reader
 
