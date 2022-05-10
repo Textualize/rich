08bd9f1286aed515ab92514ea04f2d67249fdbc8
Lanqing Huang <lqhuang@outlook.com>
2022-05-10 20:45:21 +0800

    Remove duplicated statements

--- rich/console.py
+++ rich/console.py
@@ -659,9 +659,6 @@ class Console:
             width = width or 93
             height = height or 100
 
-        self.soft_wrap = soft_wrap
-        self._width = width
-        self._height = height
         self.tab_size = tab_size
         self.record = record
         self._markup = markup
