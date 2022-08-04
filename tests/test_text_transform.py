d93e048d2f8b712dc52ad7263027c0a046d7d4e2
Dave Pearson <davep@davep.org>
2022-08-04 22:04:39 +0100

    Add text transformation tests for a non-text child renderable
    Here, as a test for other non-obviously-text items, I add testing of Panels
    and the text within.

--- tests/test_text_transform.py
+++ tests/test_text_transform.py
@@ -24,4 +24,12 @@ def test_colour_text() -> None:
     assert _as_text( Lower( COLOUR_TEXT ) ) == _as_text( COLOUR_TEXT ).lower()
     assert _as_text( SwapCase( COLOUR_TEXT ) ) == _as_text( COLOUR_TEXT ).swapcase()
 
+PANEL_TEXT   = "Halt!"
+PANEL_RESULT = f"╭────────╮\n│ {PANEL_TEXT}  │\n╰────────╯"
+
+def test_panel_text() -> None:
+    assert _as_text( Upper( Panel( PANEL_TEXT ) ), width=10 ) == PANEL_RESULT.upper()
+    assert _as_text( Lower( Panel( PANEL_TEXT ) ), width=10 ) == PANEL_RESULT.lower()
+    assert _as_text( SwapCase( Panel( PANEL_TEXT ) ), width=10 ) == PANEL_RESULT.swapcase()
+
 ### test_text_transform.py ends here
