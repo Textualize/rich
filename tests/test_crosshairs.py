ff40ca4beb85ff81541c4333e3638ad573506876
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-08-04 21:00:02 +0100

    Fix tests by adding trailing newlines.
    This commit fixes the tests by making sure the expected crosshairs outputs include the trailing newline that comes with the crosshairs.
    We also fix a sizing issue in that 'pytest' creates consoles that are 1 unit of width too short unless we specify legacy_windows=False when instantiating the console.

--- tests/test_crosshairs.py
+++ tests/test_crosshairs.py
@@ -45,28 +45,32 @@ CROSSHAIRS_DIAGRAMS = """
 │  
 ├──
 │  
-""".strip().split(
+""".strip(
+    "\n"
+).split(
     "\n\n"
 )
 
 
 def test_crosshairs():
-    console = Console(width=4, height=3)
+    console = Console(width=4, height=3, legacy_windows=False)
+    console.print(Crosshairs(1, 1))
     console.begin_capture()
     console.print(Crosshairs(1, 1))
-    assert console.end_capture() == " │  \n─┼──\n │  "
+    assert console.end_capture() == " │  \n─┼──\n │  \n"
 
 
 def test_crosshairs_edge_cases():
     """Test crosshairs creation when cross centre is flush with edges or corners."""
 
-    console = Console(width=3, height=3)
+    console = Console(width=3, height=3, legacy_windows=False)
     for diagram in CROSSHAIRS_DIAGRAMS:
         # Parse the diagram ...
         coords, output = diagram.split("\n", maxsplit=1)
         x, y = map(int, coords.split(","))
-
-        # and actually run the test.
+        # ... add the final newline...
+        output += "\n"
+        # ... and actually run the test.
         console.begin_capture()
         console.print(Crosshairs(x, y))
         assert console.end_capture() == output
