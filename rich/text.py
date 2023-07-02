e691f0cc7f41203e065f02436fbf9283c21f7b0e
Marcus Kellerman <sharkus@gmail.com>
2023-07-02 15:00:13 -0700

    Added __lt__ function to Text class to enable sort functionality

--- rich/text.py
+++ rich/text.py
@@ -171,6 +171,13 @@ class Text(JupyterMixin):
             return NotImplemented
         return self.plain == other.plain and self._spans == other._spans
 
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, str):
+            return self.plain < other
+        elif isinstance(other, Text):
+            return self.plain < other.plain
+        return False
+
     def __contains__(self, other: object) -> bool:
         if isinstance(other, str):
             return other in self.plain
@@ -1088,7 +1095,6 @@ class Text(JupyterMixin):
         _Span = Span
 
         for span_start, span_end, style in self._spans:
-
             lower_bound = 0
             upper_bound = line_count
             start_line_no = (lower_bound + upper_bound) // 2
