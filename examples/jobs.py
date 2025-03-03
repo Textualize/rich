b15604a1b54c82be37571c5b3a3b8d237e9c78cb
Matt Seddon <mattseddon@hotmail.com>
2025-03-03 13:25:17 +1100

    Update jobs example

--- examples/jobs.py
+++ examples/jobs.py
@@ -6,8 +6,8 @@ from rich.progress import Progress
 JOBS = [100, 150, 25, 70, 110, 90]
 
 progress = Progress(auto_refresh=False)
-master_task = progress.add_task("overall", total=sum(JOBS))
 jobs_task = progress.add_task("jobs")
+master_task = progress.add_task("Overall", total=sum(JOBS))
 
 progress.console.print(
     Panel(
@@ -20,12 +20,13 @@ with progress:
     for job_no, job in enumerate(JOBS):
         progress.log(f"Starting job #{job_no}")
         sleep(0.2)
-        progress.reset(jobs_task, total=job, description=f"job [bold yellow]#{job_no}")
+        progress.reset(jobs_task, total=job, description=f"Job [bold yellow]#{job_no}")
         progress.start_task(jobs_task)
         for wait in progress.track(range(job), task_id=jobs_task):
             sleep(0.01)
         progress.advance(master_task, job)
-        progress.log(f"Job #{job_no} is complete")
-    progress.log(
-        Panel(":sparkle: All done! :sparkle:", border_style="green", padding=1)
-    )
+        progress.log(f"Finished job #{job_no}")
+
+progress.console.print(
+    Panel(":sparkle: All done! :sparkle:", border_style="green", padding=1)
+)
