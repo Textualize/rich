9ec5d2969530a6b2c4da354444189416f0a767be
Chimdumebi Nebolisa <dumebineb@gmail.com>
2026-02-01 06:00:26 -0600

    style: format zwj cell_len regression test (refs #3947)

---
+++ tests/test_zwj_cell_len.py
@@ -0,0 +1,8 @@
+﻿from rich.cells import cell_len
+
+
+def test_cell_len_zwj_does_not_raise() -> None:
+    cell_len("\u200d")
+    cell_len("a\u200d")
+    cell_len("a\u200d\n b")
+    cell_len("a\u200d b(")
