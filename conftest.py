4ed692654decb9eeeea38ff11a2da35a2539d48d
Erik Sirborg <e.sirborg@gmail.com>
2026-02-18 18:48:01 +0100

    fix: removed the flags

--- conftest.py
+++
@@ -1,6 +0,0 @@
-def pytest_sessionfinish(session, exitstatus):
-    from rich._log_render import BRANCH_COVERAGE
-
-    print("\nBRANCH COVERAGE DIY ")
-    for i in sorted(BRANCH_COVERAGE):
-        print(f"{i}: {BRANCH_COVERAGE[i]}")
