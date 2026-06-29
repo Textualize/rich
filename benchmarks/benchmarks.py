6853707d5511b5ebde895a6f14b24381c184ba9d
Kevin Turcios <turcioskevinr@gmail.com>
2026-06-29 11:01:05 -0500

    perf(syntax): wrap syntax text directly

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -110,6 +110,22 @@ class SyntaxLineNumbersWrappingSuite:
         self.console.print(self.syntax, width=30)
 
 
+class SyntaxLargeWrappingSuite:
+    def setup(self):
+        self.console = Console(
+            file=StringIO(), color_system="truecolor", legacy_windows=False
+        )
+        self.syntax = Syntax(
+            code=snippets.PYTHON_SNIPPET * 120,
+            lexer="python",
+            word_wrap=True,
+            line_numbers=False,
+        )
+
+    def time_text_thin_terminal_heavy_wrapping(self):
+        self.console.print(self.syntax, width=30)
+
+
 class TableSuite:
     def time_table_no_wrapping(self):
         self._print_table(width=100)
